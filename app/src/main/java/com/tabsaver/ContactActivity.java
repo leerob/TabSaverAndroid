0e6779a5d518818536f05cb8d428b1715b5d92b3
Lee <leerob@iastate.edu>
2015-06-18 20:27:15 -0500

    Fixed the UI on the email and added error checking to the email address. Need to handle sending of message now.

--- app/src/main/java/com/tabsaver/ContactActivity.java
+++ app/src/main/java/com/tabsaver/ContactActivity.java
@@ -1,9 +1,7 @@
 package com.tabsaver;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -23,20 +21,25 @@ public class ContactActivity extends ActionBarActivity {
                 TextView email = (TextView) findViewById(R.id.contact_email);
                 TextView message = (TextView) findViewById(R.id.contact_message);
 
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("message/rfc822");
-                i.putExtra(Intent.EXTRA_EMAIL  , email.getText());
-                i.putExtra(Intent.EXTRA_SUBJECT, "TabSaver Contact Us");
-                i.putExtra(Intent.EXTRA_TEXT   , message.getText());
-                try {
-                    startActivity(Intent.createChooser(i, "Send mail..."));
-
-                    //Now finish and navigate back to settings
-                    finish();
-                    Intent settings = new Intent(getApplicationContext(), SettingsActivity.class);
-                    startActivity(settings);
-                } catch (android.content.ActivityNotFoundException ex) {
-                    Toast.makeText(ContactActivity.this, "Unable to send message. You don't have any email clients!", Toast.LENGTH_SHORT).show();
+                if(isEmailValid(email.getText())){
+//                    Intent i = new Intent(Intent.ACTION_SEND);
+//                    i.setType("message/rfc822");
+//                    i.putExtra(Intent.EXTRA_EMAIL  , email.getText());
+//                    i.putExtra(Intent.EXTRA_SUBJECT, "TabSaver Contact Us");
+//                    i.putExtra(Intent.EXTRA_TEXT   , message.getText());
+//                    try {
+//                        startActivity(Intent.createChooser(i, "Send mail..."));
+//
+//                        //Now finish and navigate back to settings
+//                        finish();
+//                        Intent settings = new Intent(getApplicationContext(), SettingsActivity.class);
+//                        startActivity(settings);
+//                    } catch (android.content.ActivityNotFoundException ex) {
+                       Toast.makeText(ContactActivity.this, "Unable to send message. You don't have any email clients!", Toast.LENGTH_SHORT).show();
+//                    }
+                }
+                else{
+                    email.setError(getString(R.string.error_invalid_email));
                 }
             }
         });
@@ -48,13 +51,15 @@ public class ContactActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    boolean isEmailValid(CharSequence email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
 }
